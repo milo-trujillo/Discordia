10cec1df2bb8a5238ca6ff7314805c3dda62496f
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2014-11-10 20:11:12 -0500

    Added documentation, files only loaded once

--- SoundPlayer.java
+++ SoundPlayer.java
@@ -4,17 +4,25 @@ import java.applet.*;
 
 public class SoundPlayer
 {
-	private static AudioClip msgReceived;
+	// Make this static so if we instantiate multiple sound players the 
+	// memory for the sound clip is only allocated once
+	private static AudioClip msgReceived = null;
+	// This is marked volatile to make it threadsafe, so playing two sounds
+	// won't yield an exception
 	private volatile boolean playing = false;
 
 	public SoundPlayer()
 	{
 		try
 		{
-			URL url = this.getClass().getResource("sounds/msg.wav");
-			String file = url.toString();
-			String urls = file.replaceFirst("file:/", "file:///");
-			msgReceived = Applet.newAudioClip(new URL(urls));
+			// Load all the sound files from disk if needed
+			if( msgReceived == null )
+			{
+				URL url = this.getClass().getResource("sounds/msg.wav");
+				String file = url.toString();
+				String urls = file.replaceFirst("file:/", "file:///");
+				msgReceived = Applet.newAudioClip(new URL(urls));
+			}
 		}
 		catch(Exception e)
 		{
@@ -26,7 +34,7 @@ public class SoundPlayer
 	{
 		try
 		{
-			if( playing )
+			if( playing ) // Abort if a sound is already playing
 				return;
 			playing = true;
 			if( s == "new message" )
