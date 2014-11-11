cd1435832449398e7484e7c6c18ed8b84ba89b43
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2014-11-10 20:10:54 -0500

    Default window size increased

--- Client.java
+++ Client.java
@@ -34,7 +34,7 @@ public class Client extends JFrame implements ActionListener
 	public Client(String host)
 	{
 		setTitle("Chat Client");
-		setSize(340, 220);
+		setSize(350, 220);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		sounds = new SoundPlayer();
